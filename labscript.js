b3d705dd1511a34df14559303885ac88189cfc34
assembly64 <saikch@kku.ac.th>
2022-04-24 16:34:56 +0700

    add new file

---
+++ labscript.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  $(".hide").hide();
+  
+  
+  $(".marker-LP").click(function(){
+    $(".hide").hide();
+    $(".LP-txt").fadeIn(300);
+  });
+  
+    $(".marker-PN").click(function(){
+       $(".hide").hide();
+    $(".PN-txt").fadeIn(300);
+  });
+  
+    $(".marker-KK1").click(function(){
+       $(".hide").hide();
+    $(".KK1-txt").fadeIn(300);
+      $(".KK2-txt").fadeIn(300);
+  });
+  
+  
+    $(".marker-RB").click(function(){
+       $(".hide").hide();
+    $(".RB-txt").fadeIn(300);
+      
+  });
+    $(".marker-SR").click(function(){
+       $(".hide").hide();
+    $(".SR-txt").fadeIn(300);
+  });
+    $(".marker-NP").click(function(){
+       $(".hide").hide();
+    $(".NP-txt").fadeIn(300);
+  });
+    $(".marker-BKK").click(function(){
+       $(".hide").hide();
+    $(".BKK-txt").fadeIn(300);
+  });
+  
+    $(".marker-NS").click(function(){
+       $(".hide").hide();
+    $(".NS-txt").fadeIn(300);
+  });
+    $(".marker-SK").click(function(){
+       $(".hide").hide();
+    $(".SK-txt").fadeIn(300);
+  });
+    $(".marker-CB").click(function(){
+       $(".hide").hide();
+    $(".CB-txt").fadeIn(300);
+      
+  });
+
+});
